4268300a6c149f37f5ad1ab5ccc300413fb1d96e
Nicolas Liampotis <nliam@grnet.gr>
2016-06-04 04:33:44 +0300

    Add SimpleSAMLphp role

---
+++ roles/ssp/files/ssha256pwgen.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+/*
+ * Script to generate password hashes.
+ *
+ */
+
+/* This is the base directory of the simpleSAMLphp installation. */
+$baseDir = $argv[1];
+
+/* Add library autoloader. */
+require_once($baseDir . '/lib/_autoload.php');
+
+$password = $argv[2];
+
+$algo = 'sha256';
+
+echo SimpleSAML\Utils\Crypto::pwHash($password, strtoupper( 'S' . $algo ) );
